c90eaf218337989159a1ac313e59f543e2206b9a
Elio Mehmeti <mehmetielio95@gmail.com>
2021-09-16 00:48:41 +0100

    adding resources and utils

---
+++ dist/utils/database.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+const { PrismaClient } = require("@prisma/client");
+const dbClient = new PrismaClient();
+exports.default = dbClient;
+//# sourceMappingURL=database.js.map
